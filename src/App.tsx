cb505e1eb3e126cf22161ba767ec05e27ea6b03c
Lasana10 <asanadaniel8@gmail.com>
2026-09-11 12:37:13 +0100

    Open the dedicated workspace for student accounts

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import ClassroomWorkspace from "./components/ClassroomWorkspace";
 import TeacherHome from "./components/TeacherHome";
 import LearningWorkspace from "./components/LearningWorkspace";
 import FamilyLearningWorkspace from "./components/FamilyLearningWorkspace";
+import StudentWorkspace from "./components/StudentWorkspace";
 import type { BootstrapStatus, CommunitySignal, Role } from "./domain/types";
 import { buildOperationalPulse } from "./domain/rules";
 import { bootstrapSchool, enrolLearner, inviteStaff, issueStudentCredential, loadBootstrapStatus, loadWorkspace, recordAssessment, recordAttendance, saveSchoolBrand, saveSchoolSetup, updateAccessStatus, updateSignalStatus, uploadSchoolLogo, type WorkspaceData } from "./lib/repository";
@@ -79,7 +80,7 @@ function WorkspaceApp() {
       {view === "admissions" && <AdmissionsView workspace={workspace} onRefresh={refreshWorkspace} onOpenLearners={()=>setView("learners")}/>}
       {view === "operations" && (workspace.viewer.role==="teacher"?<ClassroomWorkspace workspace={workspace} onRefresh={refreshWorkspace}/>:<OperationalWorkflowsView workspace={workspace} onInviteStaff={inviteStaff} onUpdateAccess={updateAccessStatus} onEnrolLearner={enrolLearner} onIssueCredential={issueStudentCredential} onRecordAttendance={recordAttendance} onRecordAssessment={recordAssessment} onRefresh={refreshWorkspace} />)}
       {view === "academics" && <AcademicOperationsView workspace={workspace} onRefresh={refreshWorkspace} onOpenStudio={()=>setView("studio")}/>} 
-      {view === "learning" && (familyLearning ? <FamilyLearningWorkspace workspace={workspace} onRefresh={refreshWorkspace}/> : <LearningWorkspace workspace={workspace} onRefresh={refreshWorkspace}/>)} 
+      {view === "learning" && (workspace.viewer.role === "student" ? <StudentWorkspace workspace={workspace} onRefresh={refreshWorkspace}/> : familyLearning ? <FamilyLearningWorkspace workspace={workspace} onRefresh={refreshWorkspace}/> : <LearningWorkspace workspace={workspace} onRefresh={refreshWorkspace}/>)}
       {view === "learners" && <LearnersWorkspace learners={workspace.learners} brand={workspace.brand} role={workspace.viewer.role} />}
       {view === "credentials" && <CredentialCardStudio workspace={workspace} onRefresh={refreshWorkspace} />}
       {view === "teachers" && <TeachersView teachers={workspace.teachers} />}
