f1c63bfe6c5dce2842d81429b9dc1d063fef9c23
Lasana10 <asanadaniel8@gmail.com>
2026-09-25 22:38:05 +0100

    Load Experience V2 once after component styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
-import "./experience-v2.css";
 import "./roleWorkspaces.css";
 import "./components/TransportManagerWorkspace.css";
 import "./components/TeacherClassroomWorkspace.css";
-import "./experience-v2.css";
 import "./connectivity.css";
+import "./experience-v2.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
